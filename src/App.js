638b2738c55849f54e71918ec7d5c220edd1da61
Henry Ly <henryly213@gmail.com>
2019-05-28 08:13:50 -0500

    add component folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="container">
+        <h1 className="text-center m-5 text-primary">Tweeter</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
-serviceWorker.unregister();
+
