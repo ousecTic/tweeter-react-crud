80c306523d484ea8df48c91fc9312a90ab290dbc
Henry Ly <henryly213@gmail.com>
2019-05-28 11:12:56 -0500

    add database and routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const config = require("./DB.js");
 
-app.listen(3000, () => {
-  console.log("server has started");
+const serverRoutes = require("./server-routes");
+
+//use section
+app.use(cors());
+// parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use("/comment", serverRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on Port: ${PORT}`);
 });
