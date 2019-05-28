e2770631ce0e8354263a3b841ca3144f2e2b69c9
Henry Ly <henryly213@gmail.com>
2019-05-28 08:37:28 -0500

    add form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Form from './components/Form.js';
 
 class App extends Component{
   render(){
     return(
       <div className="container">
         <h1 className="text-center m-5 text-primary">Tweeter</h1>
+        <Form />
       </div>
     )
   }
---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <div class="form-group">
+            <label>Username</label>
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              class="form-control"
+              placeholder="Password"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-block btn-primary mt-5">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
